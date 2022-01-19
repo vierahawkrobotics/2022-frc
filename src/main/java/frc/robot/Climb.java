3ccff272d2a7cc02618409e0720f89fac9c8d113
Hawk Robotics <Hawk Robotics@1171S-ROBOTS-06.local>
2022-01-18 19:52:16 -0500

    IMPORTANT: ADDED PHEONIX
    Rewrote climb script
    added test script
    uses talon instead of spark

--- src/main/java/frc/robot/Climb.java
+++ src/main/java/frc/robot/Climb.java
@@ -1,24 +1,54 @@
 package frc.robot;
 
-import edu.wpi.first.wpilibj.motorcontrol.*;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.Joystick;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 public class Climb {
-    public PWMSparkMax StaticArmRight;
-    public PWMSparkMax StaticArmLeft;
+    WPI_TalonSRX staticLeft;
+    WPI_TalonSRX staticRight;
+    double currentDistance;
+    Encoder encoder;
 
-    public PWMSparkMax RotateArmRight;
-    public PWMSparkMax RotateArmLeft;
+    Joystick joystick = new Joystick(0);
 
-    public Climb() {
-
+    public Climb(int joystickPort, int staticLeftPort, int staticRightPort, int encoderA, int encoderB) {
+        staticLeft = new WPI_TalonSRX(staticLeftPort);
+        staticRight = new WPI_TalonSRX(staticRightPort);
+        joystick = new Joystick(joystickPort);
+        encoder = new Encoder(encoderA, encoderB);
     }
 
     public void ClimbInit() {
-        
+        staticRight.configFactoryDefault();
+        staticLeft.configFactoryDefault();
+        staticRight.configNeutralDeadband(0.1);
+        staticLeft.configFactoryDefault();
+        staticLeft.setInverted(true);
+        staticLeft.follow(staticRight);
+
+        //256 pulses per rotation
+        encoder.setDistancePerPulse(4./256.);
+        encoder.setMaxPeriod(.1);
+        encoder.setMinRate(10);
+        encoder.setReverseDirection(false);
+        encoder.setSamplesToAverage(5);
     }
 
-    public void ArmInputUpdate(double deltaStaticArm, double deltaRotateArm) {
+    public void ClimbTestIter() {
+        double a = joystick.getRawAxis(0);
+        SetDistance(a, staticRight);
+    }
 
+    //DO NOT USE-- USE PID CONTROLLER INSTEAD
+    void SetDistance(double distance, WPI_TalonSRX motor) {
+        double speed = (currentDistance - distance);
+        speed = Math.max(Math.min(speed*10, 1),-1);
+        
+        motor.set(speed);
+
+        double dis = encoder.getDistance();
+        currentDistance += dis;
     }
 
     //State change
