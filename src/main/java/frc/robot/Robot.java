49538b85819fcf5a90810da4a4c59b4032e49f21
Pranav Ponnusamy <pranavponns@gmail.com>
2022-01-19 17:25:35 -0500

    Added pid from example code

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -10,6 +10,11 @@ import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.ctre.phoenix.motorcontrol.InvertType;
 
+import com.kauailabs.navx.frc.AHRS;
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.DriverStation;
+
 
 /**
  * This is a demo program showing the use of the DifferentialDrive class. Runs the motors with
@@ -24,7 +29,17 @@ public class Robot extends TimedRobot {
 
   public static WPI_TalonSRX rightDriveMotor1 = new WPI_TalonSRX(2);
   public static WPI_TalonSRX rightFollower = new WPI_TalonSRX(4);
+
+  private static final double driveP = 0.03;
+  private static final double driveI = 0.00;
+  private static final double driveD = 0.00;
+  private static final double driveToleranceDegrees = 1.0f;
   
+
+  PIDController turnController;
+  AHRS ahrs;
+
+
   @Override
   public void robotInit() {
     // We need to invert one side of the drivetrain so that positive voltages
@@ -36,13 +51,34 @@ public class Robot extends TimedRobot {
 
     rightFollower.setInverted(InvertType.FollowMaster);
     leftFollower.setInverted(false);
-  }
+
+
+    try { // attempt to instantiate the NavX2. If it throws an exception, catch it and
+      // report it.
+  ahrs = new AHRS(SPI.Port.kMXP); // SPI is the protocol on the MXP connector that the navigator is plugged into
+} catch (RuntimeException ex) {
+  DriverStation.reportError("Error instantiating navX2 MXP:  " + ex.getMessage(), true);
+}
+
+  turnController = new PIDController(driveP, driveI, driveD);
+  turnController.enableContinuousInput(-180.0f, 180.0f);
+  turnController.setTolerance(driveToleranceDegrees);
+  ahrs.zeroYaw();
+  turnController.setSetpoint(ahrs.getYaw());
+
+}
 
   @Override
   public void teleopPeriodic() {
     // Drive with arcade drive.
     // That means that the Y axis drives forward
     // and backward, and the X turns left and right.
-    m_robotDrive.arcadeDrive(-joystick.getY(), joystick.getZ());
+
+    double rotateToAngleRate = turnController.calculate(ahrs.getYaw(), 0.0);
+    double forward = 1.0 * joystick.getY(); // Sign this so forward is positive
+    double turn = -0.5 * joystick.getZ();
+    m_robotDrive.arcadeDrive(forward, turn + rotateToAngleRate);
+  
+  
   }
 } 
