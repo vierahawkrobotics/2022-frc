340d1563644bd9b25a114121ac9e7e99bfbe47cf
Hawk Robotics <Hawk Robotics@1171S-ROBOTS-06.local>
2022-02-26 11:08:33 -0500

    changed climb code to use stop switches

--- src/main/java/frc/robot/ClimbNoEncoder.java
+++ src/main/java/frc/robot/ClimbNoEncoder.java
@@ -1,8 +1,15 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj.DigitalInput;
+
 public class ClimbNoEncoder {
     ElevatorArm armA;
     ElevatorArm armB;
+
+    DigitalInput limit = new DigitalInput(2);
+
+    public boolean cont;
+    boolean prev;
 
     /**
      * initiates a climber without encoders
@@ -15,15 +22,36 @@ public class ClimbNoEncoder {
         armB = new ElevatorArm(canB);
         armA.maxSpeed = maxSpeed;
         armB.maxSpeed = maxSpeed;
+        cont = true;
+    }
+
+    public void StopMotors() {
+        armA.Stop();
+        armB.Stop();
     }
     
     /**
-     * sets the speed of the motors
+     * sets the speed of the motors, does not set when 'cont' is false
      * @param speedA the speed of the first arm
      * @param speedB the speed of the second arm
      */
     public void SetMotors(double speedA, double speedB) {
+        if(!cont) {
+            StopMotors();
+            return;
+        }
         armA.SetSpeed(speedA);
         armB.SetSpeed(speedB);
+
+        boolean next = ReadSensor();
+        
+        if(prev && !next) {
+            cont = false;
+        }
+        prev = next;
+    }
+
+    boolean ReadSensor() {
+        return limit.get();
     }
 }
