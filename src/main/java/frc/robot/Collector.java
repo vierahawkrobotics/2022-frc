25eca49a154442c67e94fbf13e63fd7b77f1d9d5
Hawk Robotics <Hawk Robotics@1171S-ROBOTS-06.local>
2022-03-05 11:00:54 -0500

    Added servo arm for collector

--- src/main/java/frc/robot/Collector.java
+++ src/main/java/frc/robot/Collector.java
@@ -3,6 +3,7 @@ package frc.robot;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Servo;
 
 public class Collector {
     Joystick m_stick = new Joystick(0);
@@ -10,6 +11,8 @@ public class Collector {
     private WPI_TalonSRX bottomFeeder = new WPI_TalonSRX(1);
     private WPI_TalonSRX topFeeder = new WPI_TalonSRX(2);
     double speed = .1;
+    Servo servoArm;
+    public ServoState servoState;
     /**
      * @return if the ball should move up the conveyor belt or not
      */
@@ -23,4 +26,35 @@ public class Collector {
             topFeeder.set(0);
         }
     }
+
+    public void UpdateServo() {
+
+        switch(servoState) {
+            case Close:
+                servoArm.set(.5);
+                break;
+            case Open:
+            default:
+                servoArm.set(0);
+                break;
+            
+        }
+    }
+
+    /**
+     * teleop for Collector, needs to be called every teleop period
+     * @param OpenServo opens servo
+     * @param CloseServo closes servo
+     */
+    public void CollectorTeleop(boolean OpenServo, boolean CloseServo) {
+        if(OpenServo) servoState = ServoState.Open;
+        if(CloseServo) servoState = ServoState.Close;
+
+        UpdateServo();
+    }
+}
+
+enum ServoState {
+    Open,
+    Close,
 }
