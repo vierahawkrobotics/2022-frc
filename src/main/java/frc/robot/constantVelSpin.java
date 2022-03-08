f8d89bb17abccd1941cb7cc91aec83f0c7143258
Hawk Robotics <Hawk Robotics@1171S-ROBOTS-06.local>
2022-03-07 19:18:01 -0500

    Reorganised shooting code, added sequence of operations for auto shooting

--- src/main/java/frc/robot/constantVelSpin.java
+++ src/main/java/frc/robot/constantVelSpin.java
@@ -21,7 +21,6 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
  * it contains the code necessary to operate a robot with tank drive.
  */
 public class constantVelSpin {
-  private XboxController m_stick;
   private CANSparkMax m_motor;
   private SparkMaxPIDController m_pidController;
   private RelativeEncoder m_encoder;
@@ -29,21 +28,21 @@ public class constantVelSpin {
   private boolean invert;
   Lemonlight ElisLemons = new Lemonlight();
 
-  public constantVelSpin(CANSparkMax curMotor, XboxController curStick, boolean curInvert) {
+  public constantVelSpin(CANSparkMax curMotor, boolean curInvert) {
     m_motor = curMotor;
-    m_stick = curStick;
     invert = curInvert;
   }
+
   /**
    * 
    * @return the angular velocity necessary
    */
   public double getAngularV() {
-      double linearV = ElisLemons.getVelocity();
-      double angularV = 0;
-      double radius = 2; //inches
-      angularV= linearV/radius; //rad/sec
-      return angularV;
+    double linearV = ElisLemons.getVelocity();
+    double angularV = 0;
+    double radius = 2; // inches
+    angularV = linearV / radius; // rad/sec
+    return angularV;
   }
 
   /**
@@ -51,32 +50,33 @@ public class constantVelSpin {
    * 
    * @return returns an RPM by converting Angular Velocity to it
    */
-  public double VtoRPM (){
-    double RPM =0;
+  public double VtoRPM() {
+    double RPM = 0;
     double angularV = getAngularV();
-    RPM = (60*angularV)/(2*Math.PI);
+    RPM = (60 * angularV) / (2 * Math.PI);
     System.out.println("DOGE: " + RPM);
     return RPM;
   }
+
   //
-  public void manualShooter(double RPM){
-    m_motor.set(RPM/5000);
-    }
 
   /**
-   * Puts PID coefficients on SmartDashboard 
+   * Puts PID coefficients on SmartDashboard
    */
   public void motorInit() {
     // initialize motor
     /**
-     * The RestoreFactoryDefaults method can be used to reset the configuration parameters
-     * in the SPARK MAX to their factory default state. If no argument is passed, these
+     * The RestoreFactoryDefaults method can be used to reset the configuration
+     * parameters
+     * in the SPARK MAX to their factory default state. If no argument is passed,
+     * these
      * parameters will not persist between power cycles
      */
     m_motor.restoreFactoryDefaults();
 
     /**
-     * In order to use PID functionality for a controller, a SparkMaxPIDController object
+     * In order to use PID functionality for a controller, a SparkMaxPIDController
+     * object
      * is constructed by calling the getPIDController() method on an existing
      * CANSparkMax object
      */
@@ -84,14 +84,14 @@ public class constantVelSpin {
 
     // Encoder object created to display position values
     m_encoder = m_motor.getEncoder();
-    ElisLemons.LemonTest();
+    ElisLemons.initTheLemon();
     // PID coefficients
-    kP = 0; //6e-5; 
+    kP = 0; // 6e-5;
     kI = 0;
-    kD = 0; 
-    kIz = 0; 
-    kFF = 0.000015; 
-    kMaxOutput = 1; 
+    kD = 0;
+    kIz = 0;
+    kFF = 0.000015;
+    kMaxOutput = 1;
     kMinOutput = -1;
     maxRPM = 5000;
 
@@ -114,76 +114,82 @@ public class constantVelSpin {
     SmartDashboard.putNumber("Max Output", kMaxOutput);
     SmartDashboard.putNumber("Min Output", kMinOutput);
   }
+
   /**
    * Uses PID to make sure motors are spinning at the same speed
    * The function also inverts the motor based on the constructor
    * Gets PID from SmartDashboard
    * Also sets speeds for the motor to spin at using velocity translation to RPM
    */
-  public void motorTeleop() {
-    // read PID coefficients from SmartDashboard
-    double p = SmartDashboard.getNumber("P Gain", 0);
-    double i = SmartDashboard.getNumber("I Gain", 0);
-    double d = SmartDashboard.getNumber("D Gain", 0);
-    double iz = SmartDashboard.getNumber("I Zone", 0);
-    double ff = SmartDashboard.getNumber("Feed Forward", 0);
-    double max = SmartDashboard.getNumber("Max Output", 0);
-    double min = SmartDashboard.getNumber("Min Output", 0);
+  public void shoot(boolean shootButton) {
+    if (shootButton) {
 
-    // if PID coefficients on SmartDashboard have changed, write new values to controller
-    if((p != kP)) { m_pidController.setP(p); kP = p; }
-    if((i != kI)) { m_pidController.setI(i); kI = i; }
-    if((d != kD)) { m_pidController.setD(d); kD = d; }
-    if((iz != kIz)) { m_pidController.setIZone(iz); kIz = iz; }
-    if((ff != kFF)) { m_pidController.setFF(ff); kFF = ff; }
-    if((max != kMaxOutput) || (min != kMinOutput)) { 
-      m_pidController.setOutputRange(min, max); 
-      kMinOutput = min; kMaxOutput = max; 
+      System.out.println("Auto SHooting is Wonwerbglink");
+      // read PID coefficients from SmartDashboard
+      double p = SmartDashboard.getNumber("P Gain", 0);
+      double i = SmartDashboard.getNumber("I Gain", 0);
+      double d = SmartDashboard.getNumber("D Gain", 0);
+      double iz = SmartDashboard.getNumber("I Zone", 0);
+      double ff = SmartDashboard.getNumber("Feed Forward", 0);
+      double max = SmartDashboard.getNumber("Max Output", 0);
+      double min = SmartDashboard.getNumber("Min Output", 0);
+
+      // if PID coefficients on SmartDashboard have changed, write new values to
+      // controller
+      if ((p != kP)) {
+        m_pidController.setP(p);
+        kP = p;
+      }
+      if ((i != kI)) {
+        m_pidController.setI(i);
+        kI = i;
+      }
+      if ((d != kD)) {
+        m_pidController.setD(d);
+        kD = d;
+      }
+      if ((iz != kIz)) {
+        m_pidController.setIZone(iz);
+        kIz = iz;
+      }
+      if ((ff != kFF)) {
+        m_pidController.setFF(ff);
+        kFF = ff;
+      }
+      if ((max != kMaxOutput) || (min != kMinOutput)) {
+        m_pidController.setOutputRange(min, max);
+        kMinOutput = min;
+        kMaxOutput = max;
+      }
+
+      // double setPoint = VtoRPM();
+      double setPoint = VtoRPM() / 5000;
+      if (invert) {
+        setPoint = -setPoint;
+      }
+      m_pidController.setReference(setPoint, CANSparkMax.ControlType.kVelocity);
+      // this is just to make stuff spin
+      m_motor.set(setPoint);
+
+      // this is just to integrate limelight with it
+      // m_motor.set(VtoRPM());
+
+      // System.out.println(maxRPM);
+      // System.out.println(m_stick.getRawAxis(1));
+      SmartDashboard.putNumber("SetPoint", setPoint);
+      SmartDashboard.putNumber("ProcessVariable", m_encoder.getVelocity());
+
+    } else {
+      System.out.println("NO SHOOT NO SHOOT");
+      m_motor.stopMotor();
     }
-
-    /**
-     * PIDController objects are commanded to a set point using the 
-     * SetReference() method.
-     * 
-     * The first parameter is the value of the set point, whose units vary
-     * depending on the control type set in the second parameter.
-     * 
-     * The second parameter is the control type can be set to one of four 
-     * parameters:
-     *  com.revrobotics.CANSparkMax.ControlType.kDutyCycle
-     *  com.revrobotics.CANSparkMax.ControlType.kPosition
-     *  com.revrobotics.CANSparkMax.ControlType.kVelocity
-     *  com.revrobotics.CANSparkMax.ControlType.kVoltage
-     */
-
-    //double setPoint = VtoRPM();
-    double setPoint = VtoRPM()/5000;
-    if (invert) {
-      setPoint = -setPoint;
-    }
-    m_pidController.setReference(setPoint, CANSparkMax.ControlType.kVelocity);
-
-    //this is just to make stuff spin
-    m_motor.set(setPoint);
-
-    //this is just to integrate limelight with it
-    //m_motor.set(VtoRPM());
-
-    //System.out.println(maxRPM);
-    //System.out.println(m_stick.getRawAxis(1));
-    SmartDashboard.putNumber("SetPoint", setPoint);
-    SmartDashboard.putNumber("ProcessVariable", m_encoder.getVelocity());
   }
 
   /**
    * 
    * @return encoder values
    */
-  public double getEncoder(){
+  public double getEncoder() {
     return m_encoder.getVelocity();
-  }
-
-  public void stop(){
-    m_motor.set(0);
   }
 }
